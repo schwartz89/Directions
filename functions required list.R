ffbaddfa11614c4fcccf57144b014689839df638
schwartz89 <jonnie_schwartz@hotmail.com>
2017-05-11 17:49:25 +1000

    created migration function

--- functions required list.R
+++ functions required list.R
@@ -19,7 +19,7 @@
 # Really you’re just renaming.
 #
 
-DataTransfer <- function(X,oldnames,newnames){
+MC.Setnames <- function(X,oldnames,newnames){
  #Package Installation
   Required.packages <- c("data.table")
   Missing.packages <- Required.packages[!(Required.packages %in% installed.packages()[ , "Package"])] 
@@ -38,10 +38,42 @@ DataTransfer <- function(X,oldnames,newnames){
   #scrap all non-renamed data? Or do this externally?
  #This could be done by using newnames variable, or doing a list compare
  #between original and new docs and deleting replicates
+  
+  #change input arg to a 2 column matrix? Or a GUI-selected excel doc?
 }
 
+#-------------
+# F3B Function to move data, whilst row-matching by participant ID.
+# The previous function does not do this, it only renames. This is fine
+# if all data comes from the same Comunicare report. However the data
+# is sourced from multiple reports, which do NOT match up row for row
+# and are different row lengths.
+
+MC.Movedata<-function(CCdata, MCtemplate){
+#This function moves and binds data from one dataset to another. By default 
+#it uses common columns to index by (ideally an ID), this can be specified.
+#since colnames must match for this to be useful, this function works in
+#tandem with the above.
+  
+  x <- full_join(MCtemplate, CCdata) #by = "PID") #this is optional, should do it auto, can specify multiple cols to index by if needed (for validation)
+  x
+  
+  
+  
+#TODO combine with above function?
+  
+  
+  #setkey() #data.table
+  #select() #dplyr
+  
+  ##mutating join #see dplyr two-table vignette
+    #full_join() joining by: index 
+    #semi_join()
+  }
 
 
+
+#------------
 # F4. Export function. Grabs data and prints it to appropriate .csv. 
 # Structures/dataframe naming convention in F3 will likely help here.
 
